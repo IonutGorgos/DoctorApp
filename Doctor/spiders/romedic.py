5c196564c0bdeed44630bf6cd37b0440d574a291
shopwareBot <contact@shopware.com>
2017-04-09 15:09:00 +0300

    #1 Specializari

--- Doctor/spiders/romedic.py
+++ Doctor/spiders/romedic.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.http import Request
+import re
 
 
 class RomedicSpider(scrapy.Spider):
     name = "romedic"
     allowed_domains = ["romedic.com"]
-    start_urls = ['http://romedic.com/']
+    start_urls = ['http://www.romedic.ro/medici.php']
 
     def parse(self, response):
-        pass
+        specializari = response.xpath('//ul[@class="col2v1"]')
+        for specializare in specializari:
+            spec = str(specializare.xpath('.//li/a/text()').extract())
+            # spec = re.sub(r'\s{3,}|[\r\n\t]', ' ', spec)
+            link = specializare.xpath('//li/a/@href').extract()
+            if link is not None:
+                yield Request(response.urljoin(link))
+            yield {
+                'Specializare': spec,
+                'Link': link,
+            }
